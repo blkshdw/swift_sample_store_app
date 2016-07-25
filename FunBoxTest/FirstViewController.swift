3f78d10277dca9808a26b953572adb17804eb727
blkshdw <aleksey.glebow@gmail.com>
2016-07-25 17:36:05 +0300

    Initial Commit

---
+++ FunBoxTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FunBoxTest
+//
+//  Created by Алексей on 25.07.16.
+//  Copyright © 2016 Алексей. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
